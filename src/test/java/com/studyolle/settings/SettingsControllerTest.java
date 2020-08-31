c60b69a4e36ae91e3a3ec342b77846a82dc55b1d
we <robin00q@naver.com>
2020-08-31 15:19:40 +0900

    Update password Test

--- src/test/java/com/studyolle/settings/SettingsControllerTest.java
+++ src/test/java/com/studyolle/settings/SettingsControllerTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,6 +33,9 @@ class SettingsControllerTest {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @AfterEach
     void afterEach() {
@@ -66,7 +71,7 @@ class SettingsControllerTest {
     @WithAccount("sjlee")
     @DisplayName("프로필 수정 하기 - 입력값 에러")
     @Test
-    void updateProfile_error() throws Exception {
+    void updateProfile_fail() throws Exception {
         String bio = "길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우, 길게 작성하는 경우";
         mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
                 .param("bio", bio)
@@ -79,6 +84,50 @@ class SettingsControllerTest {
 
         Account sjlee = accountRepository.findByNickname("sjlee");
         assertNull(sjlee.getBio());
+    }
+
+    @WithAccount("sjlee")
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    void updatePassword_form() throws Exception {
+        mockMvc.perform(get(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"));
+    }
+
+    @WithAccount("sjlee")
+    @DisplayName("패스워드 수정 - 입력값 정상")
+    @Test
+    void updatePassword_success() throws Exception {
+        String newPassword = "12345678";
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                    .with(csrf())
+                    .param("newPassword", newPassword)
+                    .param("newPasswordConfirm", newPassword))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:" + SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        assertTrue(passwordEncoder.matches(
+                "12345678",
+                accountRepository.findByNickname("sjlee").getPassword()));
+    }
+
+    @WithAccount("sjlee")
+    @DisplayName("패스워드 수정 - 입력값 에러 - 패스워드 불일치")
+    @Test
+    void updatePassword_fail() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                    .with(csrf())
+                    .param("newPassword", "12345678")
+                    .param("newPasswordConfirm", "11111111"))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"));
     }
 
 }
