2bcf4e608c66c9cac9d2b2098dbd78ab26ecf63b
we <robin00q@naver.com>
2020-09-03 18:39:38 +0900

    Event Notification

---
+++ src/main/java/com/studyolle/modules/study/event/event/EnrollmentEvent.java
@@ -0,0 +1,15 @@
+package com.studyolle.modules.study.event.event;
+
+import com.studyolle.modules.event.Enrollment;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public abstract class EnrollmentEvent {
+
+    protected final Enrollment enrollment;
+
+    protected final String message;
+
+}
